5e5488f805735f6735a984c9c2dab7021be2a9ad
Стас Басов <stas.basov@whatever.com>
2023-08-15 11:04:55 +0300

    fef

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,9 +2,16 @@
 import os
 from pathlib import Path
 
+import dotenv
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
